ec4290aad1cbacb549fd7ac8e3d48d7311879328
Yawuyawu <yawuyawu@gmail.com>
2026-06-05 16:21:47 +0100

    move html css js to public folder

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.static('public'));
-
-// Pas de base de données, juste une variable
-let points = 1;
-
-app.get('/api/points', (req, res) => {
-  res.json({ points });
-});
-
-app.post('/api/points', (req, res) => {
-  points = req.body.points;
-  res.json({ success: true, points });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+git add .
+git commit -m "server.js complet sans vercel"
+git push
